a361bd8c90d7430880bacf08f70662f7fc4efe59
parthpuneet03 <83951620+parthpuneet03@users.noreply.github.com>
2022-05-01 23:10:28 +0530

    Create ceasar_cypher.py

---
+++ ceasar_cypher.py
@@ -0,0 +1,39 @@
+def encrypt(text,shift):
+  
+  lst=[]
+  for i in text:
+    i=ord(i)
+    i=i+shift
+    if(i>122):
+        i=96+(i-122)
+    lst.append(i)
+    print(i)
+  str=""
+  for i in range(len(lst)):
+    lst[i]=chr(lst[i])
+    str+=lst[i]
+  return str
+  
+def decrypt(text,shift):
+  lst=[]
+  for i in text:
+    i=ord(i)
+    i=i-shift
+    if(i<97):
+        i=122-(96-i)
+    lst.append(i)
+  str=""
+  for i in range(len(lst)):
+    lst[i]=chr(lst[i])
+    str+=lst[i]
+  return str
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+if( direction=="encode"):
+    encrypted_txt=encrypt(text,shift)
+    print(f"The encoded text is {encrypted_txt}")
+else:
+    decrypted_txt=decrypt(text,shift)
+    print(f"The decoded text is {decrypted_txt}")
